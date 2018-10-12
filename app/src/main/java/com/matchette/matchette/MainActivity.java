fd878538534b31783e14f649ef92c9ba6bc57188
Onur Unal <ou.unal@gmail.com>
2018-10-11 23:35:02 -0500

    The buttons with animation, but the animation is buggy atm. Resizing is there.

--- app/src/main/java/com/matchette/matchette/MainActivity.java
+++ app/src/main/java/com/matchette/matchette/MainActivity.java
@@ -1,22 +1,42 @@
 package com.matchette.matchette;
 
+import android.animation.LayoutTransition;
 import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.animation.TranslateAnimation;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    //A variable that says if the snackbar is down (invisible) or not.
-    boolean isDown = true;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //This is what I tried to fix the buggy animation but it doesn't work.
+        /*final ViewGroup wholeLayout = findViewById(R.id.whole_layout);
+        LayoutTransition transitioner1 = wholeLayout.getLayoutTransition();
+        transitioner1.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0);
+        transitioner1.setStartDelay(LayoutTransition.APPEARING, 0);
+        transitioner1.setStartDelay(LayoutTransition.DISAPPEARING, 0);
+
+        final ViewGroup frame = findViewById(R.id.temporary_frame);
+        LayoutTransition transitioner2 = frame.getLayoutTransition();
+        transitioner2.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0);
+        transitioner2.setStartDelay(LayoutTransition.APPEARING, 0);
+        transitioner2.setStartDelay(LayoutTransition.DISAPPEARING, 0);
+
+        final ViewGroup snackbar = findViewById(R.id.custom_snackbar);
+        LayoutTransition transitioner3 = snackbar.getLayoutTransition();
+        transitioner3.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0);
+        transitioner3.setStartDelay(LayoutTransition.APPEARING, 0);
+        transitioner3.setStartDelay(LayoutTransition.DISAPPEARING, 0);*/
 
         //Temporary buttons 1 and 2, the one in the middle and the one at the bottom. Should show/hide the custom snackbar.
         final Button tempButton1 = findViewById(R.id.temporary_button);
@@ -24,62 +44,73 @@ public class MainActivity extends AppCompatActivity {
 
 
         //This is the custom snackbar that is set to be GONE at creation.
-        final LinearLayout snackBar = (LinearLayout)this.findViewById(R.id.custom_snackbar);
-
+        final LinearLayout snackBar = this.findViewById(R.id.custom_snackbar);
 
         /*Set button 1 behavior, not satisfied by this but it's a start. It forces me to have a View parameter
         in the onClick method override.
          */
-        tempButton1.setOnClickListener( new View.OnClickListener()
-        {
+        tempButton1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
-                if (isDown) {
-                    show_layout(snackBar);
-                    isDown=false;
+            public void onClick(View v) {
+                if (snackBar.getVisibility()==LinearLayout.GONE) {
+                    snackBar.setVisibility(LinearLayout.VISIBLE);
+                    //show_layout(snackBar);
+
                 } else {
-                    hide_layout(snackBar);
-                    isDown=true;
+                    //hide_layout(snackBar);
+                    snackBar.setVisibility(LinearLayout.GONE);
                 }
             }
         });
 
         //Set the Button 2 behavior.
-        tempButton2.setOnClickListener( new View.OnClickListener()
-        {
+        tempButton2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
+            public void onClick(View v) {
+                if (snackBar.getVisibility() != LinearLayout.GONE) {
+                    //hide_layout(snackBar);
+                    snackBar.setVisibility(LinearLayout.GONE);
+                }
 
-                hide_layout(snackBar);
-                isDown=true;
             }
         });
 
 
-
     }
 
-    /**
+    //This is from when I tried to implement custom animation, but it's buggier as is.
+/*    *//**
      * Shows the custom snackbar with animation.
+     *
      * @param l is the LinearLayout you want to be visible.
-     */
+     *//*
     public void show_layout(LinearLayout l) {
-        ObjectAnimator animation = ObjectAnimator.ofFloat(l, "translationY", l.getHeight());
-        animation.setDuration(1000);
-        animation.start();
-        l.setVisibility(LinearLayout.VISIBLE);
+        final int screenHeight = getScreenHeight();
+        ObjectAnimator animator = ObjectAnimator.ofFloat(l, "y", screenHeight, (screenHeight * 0.6F));
+        animator.setInterpolator(new DecelerateInterpolator());
+        animator.start();
     }
 
-    /**
+    *//**
      * Hides the custom snackbar with animation.
+     *
      * @param l is the LinearLayout you want to be hidden.
-     */
+     *//*
     public void hide_layout(LinearLayout l) {
-        ObjectAnimator animation = ObjectAnimator.ofFloat(l, "translationY", -l.getHeight());
-        animation.setDuration(1000);
-        animation.start();
-        l.setVisibility(LinearLayout.GONE);
+        final int screenHeight = getScreenHeight();
+        ObjectAnimator animator = ObjectAnimator.ofFloat(l, "y", -screenHeight, (screenHeight * 0.6F));
+        animator.setInterpolator(new DecelerateInterpolator());
+        animator.start();
     }
 
+    *//**
+     * Gets the screen height.
+     * @return The screen height as an int.
+     *//*
+    int getScreenHeight() {
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        return displaymetrics.heightPixels;
+    }*/
 }
 
