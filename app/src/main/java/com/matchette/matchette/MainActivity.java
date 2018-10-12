47701f90de6ef4d75f21ca07a63ae8eb24ebb232
cgiang <cgiang@macalester.edu>
2018-10-12 05:19:51 -0500

    Fixed animation bug (?) and repositioned layout

--- app/src/main/java/com/matchette/matchette/MainActivity.java
+++ app/src/main/java/com/matchette/matchette/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //This is what I tried to fix the buggy animation but it doesn't work.
-        /*final ViewGroup wholeLayout = findViewById(R.id.whole_layout);
+        // For animation when showing/hiding the snackbar
+        final ViewGroup wholeLayout = findViewById(R.id.whole_layout);
         LayoutTransition transitioner1 = wholeLayout.getLayoutTransition();
         transitioner1.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0);
         transitioner1.setStartDelay(LayoutTransition.APPEARING, 0);
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         LayoutTransition transitioner3 = snackbar.getLayoutTransition();
         transitioner3.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0);
         transitioner3.setStartDelay(LayoutTransition.APPEARING, 0);
-        transitioner3.setStartDelay(LayoutTransition.DISAPPEARING, 0);*/
+        transitioner3.setStartDelay(LayoutTransition.DISAPPEARING, 0);
 
         //Temporary buttons 1 and 2, the one in the middle and the one at the bottom. Should show/hide the custom snackbar.
         final Button tempButton1 = findViewById(R.id.temporary_button);
@@ -54,10 +54,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (snackBar.getVisibility()==LinearLayout.GONE) {
                     snackBar.setVisibility(LinearLayout.VISIBLE);
-                    //show_layout(snackBar);
+                    show_layout(snackBar);
 
                 } else {
-                    //hide_layout(snackBar);
+                    hide_layout(snackBar);
                     snackBar.setVisibility(LinearLayout.GONE);
                 }
             }
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (snackBar.getVisibility() != LinearLayout.GONE) {
-                    //hide_layout(snackBar);
+                    hide_layout(snackBar);
                     snackBar.setVisibility(LinearLayout.GONE);
                 }
 
@@ -78,39 +78,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //This is from when I tried to implement custom animation, but it's buggier as is.
-/*    *//**
+    //Implement custom animation -- tried to fix the bug here
+    /**
      * Shows the custom snackbar with animation.
      *
      * @param l is the LinearLayout you want to be visible.
-     *//*
+     */
     public void show_layout(LinearLayout l) {
         final int screenHeight = getScreenHeight();
-        ObjectAnimator animator = ObjectAnimator.ofFloat(l, "y", screenHeight, (screenHeight * 0.6F));
+        ObjectAnimator animator = ObjectAnimator.ofFloat(l, "y", screenHeight, (screenHeight * 0.619F));
         animator.setInterpolator(new DecelerateInterpolator());
         animator.start();
     }
 
-    *//**
+    /*
      * Hides the custom snackbar with animation.
      *
      * @param l is the LinearLayout you want to be hidden.
-     *//*
+     */
     public void hide_layout(LinearLayout l) {
         final int screenHeight = getScreenHeight();
-        ObjectAnimator animator = ObjectAnimator.ofFloat(l, "y", -screenHeight, (screenHeight * 0.6F));
+        ObjectAnimator animator = ObjectAnimator.ofFloat(l, "y", screenHeight);
         animator.setInterpolator(new DecelerateInterpolator());
         animator.start();
     }
 
-    *//**
+    /**
      * Gets the screen height.
      * @return The screen height as an int.
-     *//*
+     */
     int getScreenHeight() {
         DisplayMetrics displaymetrics = new DisplayMetrics();
         this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
         return displaymetrics.heightPixels;
-    }*/
+    }
 }
 
