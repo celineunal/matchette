0e0063a58ea440009e2f810507239f3072aece32
cgiang <cgiang@macalester.edu>
2018-10-13 12:03:15 -0500

    Changed snackbar

--- app/src/main/java/com/matchette/matchette/MainActivity.java
+++ app/src/main/java/com/matchette/matchette/MainActivity.java
@@ -2,8 +2,8 @@ package com.matchette.matchette;
 
 import android.animation.LayoutTransition;
 import android.animation.ObjectAnimator;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
