ab47d77ef2eba5318aa02dfd053f36cf53f66295
hismaeel <hismaeel@macalester.edu>
2018-10-19 08:39:53 -0800

    Fragment combined to UI

--- app/src/main/java/com/matchette/matchette/MainActivity.java
+++ app/src/main/java/com/matchette/matchette/MainActivity.java
@@ -1,33 +1,45 @@
 package com.matchette.matchette;
 
 import android.animation.LayoutTransition;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 import com.skydoves.colorpickerpreference.ColorEnvelope;
 import com.skydoves.colorpickerpreference.ColorListener;
-import com.skydoves.colorpickerpreference.ColorPickerView;
+import com.flask.colorpicker.ColorPickerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private LinearLayout snackBar;
+    private int count = 0;
     String [] shirts;
     String [] pants;
-    String shirtColor;
-    String pantColor;
-    String shirtType;
-    String pantType;
+    String currShirtColor;
+    String currPantColor;
+    String currSnackbarSelection = "shirt";
+
+    String currShirt = "t-shirt";
+    String currPant = "pants";
 
     //For the recyclerView
     private List<Style> shirtStyleList = new ArrayList<>();
@@ -35,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
     private List<Style> currentItemList = new ArrayList<>();
     private RecyclerView recyclerView;
     private StyleAdapter sAdapter;
+    private MainActivityFragment mainFragment;
+
+    protected String getCurrShirt(){
+        return this.currShirt;
+    }
+
+    protected String getCurrPant(){
+        return this.currPant;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
         transitioner1.setStartDelay(LayoutTransition.CHANGE_APPEARING, 0);
         transitioner1.setStartDelay(LayoutTransition.CHANGING, 0);
 
-
         final FrameLayout frame = findViewById(R.id.temporary_frame);
         LayoutTransition transitioner2 = frame.getLayoutTransition();
         transitioner2.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0);
@@ -61,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //This is the custom snackbar that is set to be GONE at creation.
-        final LinearLayout snackBar = this.findViewById(R.id.custom_snackbar);
+        snackBar = this.findViewById(R.id.custom_snackbar);
 
         LayoutTransition transitioner3 = snackBar.getLayoutTransition();
         transitioner3.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0);
@@ -69,11 +89,6 @@ public class MainActivity extends AppCompatActivity {
         transitioner3.setStartDelay(LayoutTransition.DISAPPEARING, 0);
         transitioner3.setStartDelay(LayoutTransition.CHANGE_APPEARING, 0);
         transitioner3.setStartDelay(LayoutTransition.CHANGING, 0);
-
-
-        //Temporary buttons representing shirt and pants. Should show/hide the custom snackbar.
-        final Button shirtButton = findViewById(R.id.shirtButton);
-        final Button pantsButton = findViewById(R.id.pantsButton);
 
 
         final ImageButton closeSnackbar = findViewById(R.id.closeSnackbar);
@@ -113,71 +128,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view, int position) {
                 Style currentStyle = currentItemList.get(position);
-                // fragment.setStyle(curentStyle, type); ??
-                Toast.makeText(getApplicationContext(), currentStyle.getName() + " " + currentStyle.getRid(), Toast.LENGTH_SHORT).show(); // temporary
+                Log.d("change", currSnackbarSelection + " | " + currentStyle.getName());
+                if (currSnackbarSelection.equals("shirt")){
+                    currShirt = currentStyle.getName();
+                    mainFragment.changeStyle("shirt", currentStyle.getName());
+                } else {
+                    currPant = currentStyle.getName();
+                    mainFragment.changeStyle("pant", currentStyle.getName());
+                }
             }
 
             @Override
             public void onLongClick(View view, int position) {}
         }));
 
-
-        /*Set the shirtButton and pantsButton behavior, not satisfied by this but it's a start. It forces me to have a View parameter
-        in the onClick method override.
-         */
-
-        shirtButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (currentItemList == shirtStyleList && snackBar.getVisibility()==LinearLayout.GONE)
-                    snackBar.setVisibility(LinearLayout.VISIBLE);
-                else if (currentItemList != shirtStyleList && snackBar.getVisibility()==LinearLayout.VISIBLE){
-                    currentItemList = shirtStyleList;
-                    recycShirt();
-                }
-                else if (currentItemList != shirtStyleList && snackBar.getVisibility()==LinearLayout.GONE){
-                    currentItemList = shirtStyleList;
-                    recycShirt();
-                    snackBar.setVisibility(LinearLayout.VISIBLE);
-                }
-            }
-        });
-
-        pantsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (currentItemList == pantsStyleList && snackBar.getVisibility()==LinearLayout.GONE)
-                    snackBar.setVisibility(LinearLayout.VISIBLE);
-                else if (currentItemList != pantsStyleList && snackBar.getVisibility()==LinearLayout.VISIBLE){
-                    currentItemList = pantsStyleList;
-                    recycPants();
-                }
-                else if (currentItemList != pantsStyleList && snackBar.getVisibility()==LinearLayout.GONE){
-                    currentItemList = pantsStyleList;
-                    recycPants();
-                    snackBar.setVisibility(LinearLayout.VISIBLE);
-                }
-            }
-        });
-
-
-        // Set the closeSnackbar behavior.
+        //Set the closeSnackbar behavior.
         closeSnackbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (snackBar.getVisibility() != LinearLayout.GONE) {
-                    //hide_layout(snackBar);
                     snackBar.setVisibility(LinearLayout.GONE);
                 }
 
             }
         });
 
-        // Set up the color picker
-        colorPicker.setColorListener(new ColorListener() {
+        colorPicker.addOnColorSelectedListener(new OnColorSelectedListener() {
             @Override
-            public void onColorSelected(ColorEnvelope colorEnvelope) {
-                frame.setBackgroundColor(colorEnvelope.getColor());
+            public void onColorSelected(int i) {
+                if (currSnackbarSelection.equals("shirt")) {
+                    mainFragment.changeColor(Integer.toHexString(i).toUpperCase(), currSnackbarSelection, currShirt);
+                } else {
+                    mainFragment.changeColor(Integer.toHexString(i).toUpperCase(), currSnackbarSelection, currPant);
+                }
             }
         });
 
@@ -198,9 +181,43 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Coming soon!", Toast.LENGTH_SHORT).show();
             }
         });
+
+        mainFragment = new MainActivityFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.add(R.id.fragment, mainFragment);
+        ft.commit();
     }
 
 
+    protected void animationLogicShirt(){
+        currSnackbarSelection = "shirt";
+        if (currentItemList == shirtStyleList && snackBar.getVisibility()==LinearLayout.GONE)
+            snackBar.setVisibility(LinearLayout.VISIBLE);
+        else if (currentItemList != shirtStyleList && snackBar.getVisibility()==LinearLayout.VISIBLE){
+            currentItemList = shirtStyleList;
+            recycShirt();
+        }
+        else if (currentItemList != shirtStyleList && snackBar.getVisibility()==LinearLayout.GONE){
+            currentItemList = shirtStyleList;
+            recycShirt();
+            snackBar.setVisibility(LinearLayout.VISIBLE);
+        }
+    }
+
+    protected void animationLogicPant(){
+        currSnackbarSelection = "pant";
+        if (currentItemList == pantsStyleList && snackBar.getVisibility()==LinearLayout.GONE)
+            snackBar.setVisibility(LinearLayout.VISIBLE);
+        else if (currentItemList != pantsStyleList && snackBar.getVisibility()==LinearLayout.VISIBLE){
+            currentItemList = pantsStyleList;
+            recycPants();
+        }
+        else if (currentItemList != pantsStyleList && snackBar.getVisibility()==LinearLayout.GONE){
+            currentItemList = pantsStyleList;
+            recycPants();
+            snackBar.setVisibility(LinearLayout.VISIBLE);
+        }
+    }
     /**
      * Set up the RecyclerView for shirts
      */
@@ -217,50 +234,27 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(sAdapter);
     }
 
-    /**
-     * Sets the shirtColor or pantColor variable.
-     * @param colorHexValue
-     * @param type Shirt or pants.
-     */
-    public void setColor (String colorHexValue, String type) {
-        if (type == "shirt"){
-            shirtColor = colorHexValue;
-        } else if (type == "pants") {
-            pantColor = colorHexValue;
-        }
-    }
-
-    /**
-     * Sets the shirtType or pantType variable.
-     * @param style
-     * @param type Shirt or pants.
-     */
-    public void setStyle (String style, String type) {
-        if (type == "shirt"){
-            shirtType= style;
-        } else if (type == "pants") {
-            pantType = style;
-        }
-    }
-
     private void prepareShirtStyles () {
-        Style style = new Style("T-shirt", R.drawable.tshirt);
+        Style style = new Style("t-shirt", R.drawable.ic_shirt);
         shirtStyleList.add(style);
 
-        style = new Style("Shirt", R.drawable.shirt);
+        style = new Style("dress-shirt", R.drawable.ic_dress_shirt);
         shirtStyleList.add(style);
 
-        style = new Style("Polo Shirt", R.drawable.polo);
+        style = new Style("hoodie", R.drawable.ic_hoodie);
+        shirtStyleList.add(style);
+
+        style = new Style("polo", R.drawable.ic_polo);
         shirtStyleList.add(style);
 
     }
 
     private void preparePantsStyles() {
-        Style style = new Style("Pants", R.drawable.pants);
+        Style style = new Style("pants", R.drawable.ic_pant);
         pantsStyleList.add(style);
 
-        style = new Style("Shorts", R.drawable.shorts);
-        pantsStyleList.add(style);
+//        style = new Style("shorts", R.drawable.ic_shorts);
+//        pantsStyleList.add(style);
     }
 
 
---
+++ app/src/main/java/com/matchette/matchette/MainActivityFragment.java
@@ -0,0 +1,188 @@
+package com.matchette.matchette;
+
+import android.animation.LayoutTransition;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.devs.vectorchildfinder.VectorChildFinder;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends android.app.Fragment {
+    View fragment;
+    ImageView shirt;
+    ImageView pant;
+    LinearLayout shirtLayout;
+    LinearLayout pantLayout;
+    LinearLayout paddingLayout;
+
+    public MainActivityFragment() {
+
+    }
+
+    protected void changeColor(String color, String type, String style){
+        switch (type){
+            case "shirt":
+                switch(style){
+                    case "t-shirt":
+                        changeColorUtil(color, "t-shirt", type, 4);
+                        break;
+                    case "polo":
+                        changeColorUtil(color, "polo", type, 1);
+                        break;
+                    case "dress-shirt":
+                        changeColorUtil(color, "dress-shirt", type, 5);
+                        break;
+                    case "hoodie":
+                        changeColorUtil(color, "hoodie", type, 1);
+                        break;
+                }
+                break;
+            case "pant":
+                switch(style){
+                    case "pants":
+                        changeColorUtil(color, "pants",type,  1);
+                        break;
+                    case "shorts":
+                        changeColorUtil(color, "shorts",type, 8);
+                        break;
+                }
+                break;
+        }
+    }
+
+    protected void changeColorUtil(String color, String style, String type, int num){
+        int id = 0;
+        switch(style){
+            case "t-shirt":
+                id = R.drawable.ic_shirt;
+                break;
+            case "polo":
+                id = R.drawable.ic_polo;
+                break;
+            case "dress-shirt":
+                id = R.drawable.ic_dress_shirt;
+                break;
+            case "hoodie":
+                id = R.drawable.ic_hoodie;
+                break;
+            case "pants":
+                id = R.drawable.ic_pant;
+                break;
+            case "shorts":
+                id = R.drawable.ic_shorts;
+                break;
+        }
+        ImageView view = (type.equals("shirt")) ? shirt:pant;
+        VectorChildFinder vector = new VectorChildFinder(getActivity(), id, view);
+        for (int i = 1; i <= num; i++){
+            vector.findPathByName("yolo" + i).setFillColor(Color.parseColor("#"+color));
+        }
+        view.invalidate();
+    }
+
+    protected void changeStyle(String type, String style){
+        Log.d("change 2", type + " | " + style);
+        switch(type){
+            case "shirt":
+                switch(style){
+                    case "dress-shirt":
+                        shirt.setImageResource(R.drawable.ic_dress_shirt);
+                        LinearLayout.LayoutParams lp1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp1.weight=1.0f;
+                        shirtLayout.setLayoutParams(lp1);
+                        LinearLayout.LayoutParams lp5 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp5.weight=0f;
+                        paddingLayout.setLayoutParams(lp5);
+                        break;
+                    case "polo":
+                        shirt.setImageResource(R.drawable.ic_polo);
+                        LinearLayout.LayoutParams lp4 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,0);
+                        lp4.weight=1.2f;
+                        shirtLayout.setLayoutParams(lp4);
+                        LinearLayout.LayoutParams lp6 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp6.weight=0f;
+                        paddingLayout.setLayoutParams(lp6);
+                        break;
+                    case "t-shirt":
+                        shirt.setImageResource(R.drawable.ic_shirt);
+                        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp2.weight=1.1f;
+                        shirtLayout.setLayoutParams(lp2);
+                        LinearLayout.LayoutParams lp7 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp7.weight=0f;
+                        paddingLayout.setLayoutParams(lp7);
+                        break;
+                    case "hoodie":
+                        //change weight here
+                        shirt.setImageResource(R.drawable.ic_hoodie);
+                        LinearLayout.LayoutParams lp3 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp3.weight=1.2f;
+                        shirtLayout.setLayoutParams(lp3);
+                        LinearLayout.LayoutParams lp8 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp8.weight=0f;
+                        paddingLayout.setLayoutParams(lp8);
+                        break;
+                }
+                break;
+            case "pant":
+                switch(style){
+                    case "pants":
+                        pant.setImageResource(R.drawable.ic_pant);
+                        LinearLayout.LayoutParams lpp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lpp.weight=1.25f;
+                        pantLayout.setLayoutParams(lpp);
+                        LinearLayout.LayoutParams lp5 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp5.weight=0f;
+                        paddingLayout.setLayoutParams(lp5);
+                        break;
+                    case "shorts":
+                        pant.setImageResource(R.drawable.ic_shorts);
+                        LinearLayout.LayoutParams lp3 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp3.weight=0.7f;
+                        pantLayout.setLayoutParams(lp3);
+                        LinearLayout.LayoutParams lp4 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                        lp4.weight=1f;
+                        paddingLayout.setLayoutParams(lp4);
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater,container,savedInstanceState);
+        fragment = inflater.inflate(R.layout.fragment_main, container, false);
+        pant = (ImageView) fragment.findViewById(R.id.pant);
+        shirt = (ImageView) fragment.findViewById(R.id.shirt);
+        shirtLayout = (LinearLayout) fragment.findViewById(R.id.shirtLayout);
+        pantLayout = (LinearLayout) fragment.findViewById(R.id.pantLayout);
+        paddingLayout = (LinearLayout) fragment.findViewById(R.id.paddingLayout);
+        shirt.setClickable(true);
+        shirt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity act = (MainActivity) getActivity();
+                act.animationLogicShirt();
+            }
+        });
+        pant.setClickable(true);
+        pant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity act = (MainActivity) getActivity();
+                act.animationLogicPant();
+            }
+        });
+        return fragment;
+    }
+}
