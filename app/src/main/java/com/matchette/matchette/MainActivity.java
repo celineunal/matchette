38c7cc4c8d3e19d948c86a1a6d9d8763523858ef
Onur Unal <ou.unal@gmail.com>
2018-11-16 15:19:15 -0600

    Recyclerview focuses now, a small change I'll work on it more tonight.

--- app/src/main/java/com/matchette/matchette/MainActivity.java
+++ app/src/main/java/com/matchette/matchette/MainActivity.java
@@ -12,7 +12,6 @@ import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -22,7 +21,9 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,12 +37,10 @@ import com.flask.colorpicker.ColorPickerView;
 import com.flask.colorpicker.OnColorChangedListener;
 import com.flask.colorpicker.OnColorSelectedListener;
 
-import org.xmlpull.v1.XmlPullParser;
+
 import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -205,13 +204,13 @@ public class MainActivity extends Activity {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        SnapHelper snapHelper = new PagerSnapHelper();
+        snapHelper.attachToRecyclerView(recyclerView);
         recyclerView.setScrollbarFadingEnabled(false); // always visible
 
         shirtStyleList = loadStyleListFromXml("shirt_styles", context);
         pantsStyleList = loadStyleListFromXml("pant_styles", context);
 
-        int poop = shirtStyleList.get(0).getRid();
-        Log.d("Rid", String.valueOf(poop));
     }
 
     private void recyclerViewListener(){
@@ -398,64 +397,6 @@ public class MainActivity extends Activity {
 
         return styles;
     }
-
-    private void prepareShirtStyles () {
-        Style style = new Style("t-shirt", R.drawable.ic_t_shirt, 1);
-        shirtStyleList.add(style);
-
-        // would crash
-//        style = new Style("dress-shirt", R.drawable.ic_dress_shirt);
-//        shirtStyleList.add(style);
-
-        style = new Style("hoodie", R.drawable.ic_hoodie, 1);
-        shirtStyleList.add(style);
-
-        style = new Style("polo", R.drawable.ic_polo, 1);
-        shirtStyleList.add(style);
-
-        style = new Style("woman-sleeveless-shirt", R.drawable.ic_woman_sleeveless_shirt, 1);
-        shirtStyleList.add(style);
-
-        style = new Style("blouse", R.drawable.ic_blouse, 1);
-        shirtStyleList.add(style);
-
-        style = new Style("man-coat", R.drawable.ic_man_coat, 1);
-        shirtStyleList.add(style);
-
-        style = new Style("man-suit", R.drawable.ic_man_suit, 1);
-        shirtStyleList.add(style);
-
-        style = new Style("woman-jacket", R.drawable.ic_woman_jacket, 1);
-        shirtStyleList.add(style);
-
-        style = new Style("woman-suit", R.drawable.ic_woman_suit, 1);
-        shirtStyleList.add(style);
-    }
-
-    private void preparePantsStyles() {
-        Style style = new Style("pants", R.drawable.ic_pant, 1);
-        pantsStyleList.add(style);
-
-        style = new Style("shorts", R.drawable.ic_shorts, 1);
-        pantsStyleList.add(style);
-
-        style = new Style("formal-pants", R.drawable.ic_formal_pants, 1);
-        pantsStyleList.add(style);
-
-        style = new Style("woman-pants", R.drawable.ic_woman_pants,1 );
-        pantsStyleList.add(style);
-
-        style = new Style("skirt", R.drawable.ic_skirt, 1);
-        pantsStyleList.add(style);
-
-        style = new Style("long-skirt", R.drawable.ic_long_skirt, 1);
-        pantsStyleList.add(style);
-
-        style = new Style("formal-skirt", R.drawable.ic_formal_skirt, 1);
-        pantsStyleList.add(style);
-    }
-
-
 
     /**
      * Sets the various start delays to 0 for automatic animation in layout changes.
